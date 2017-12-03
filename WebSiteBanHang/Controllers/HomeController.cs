68287982c2980c638c0c230bc6f46ab383dfc8cd
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-03 16:42:20 +0700

    finish bai16 -- Bai 5.1 -- HƯỚNG DẪN TRUY XUẤT LẤY DỮ LIỆU TỪ CSDL THÔNG QUA MÔ HÌNH MVC HIỂN THỊ LÊN WEBSITE

--- WebSiteBanHang/Controllers/HomeController.cs
+++ WebSiteBanHang/Controllers/HomeController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSiteBanHang.Models;
 
 namespace WebSiteBanHang.Controllers
 {
     public class HomeController : Controller
     {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+
         public ActionResult Index()
         {
+
+            // List điện thoại mới nhất
+            var lstDTM = db.SanPhams.Where(n => n.MaLoaiSP == 1 && n.Moi == 1 && n.DaXoa == false);
+            ViewBag.ListDTM = lstDTM;
+            // List laptop mới nhất 
+            var lstLTM = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1 && n.DaXoa == false);
+            ViewBag.ListLTM = lstLTM;
+            //List Máy tính bảng mới
+            var lstMTBM = db.SanPhams.Where(n => n.MaLoaiSP == 3 && n.Moi == 1 && n.DaXoa == false);
+            ViewBag.ListMTBM = lstMTBM;
             return View();
         }
+
+
 
         public ActionResult About()
         {
--- WebSiteBanHang/Controllers/SanPhamController.cs
+++ WebSiteBanHang/Controllers/SanPhamController.cs
@@ -10,30 +10,16 @@ namespace WebSiteBanHang.Controllers
     public class SanPhamController : Controller
     {
         QuanLyBanHangEntities db = new QuanLyBanHangEntities();
-        public ActionResult SanPham1()
-        {
-            var listSanPhamLTM = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
-            ViewBag.ListLT = listSanPhamLTM;
 
-            var listSanPhamDT = db.SanPhams.Where(n => n.MaLoaiSP == 1);
-            ViewBag.ListDT = listSanPhamDT;
-
-            var listSanPhamLMT = db.SanPhams.Where(n => n.MaLoaiSP == 3);
-            ViewBag.ListMT = listSanPhamLMT;
-            return View();
-        }
-
-        public ActionResult SanPham2()
-        {
-            var listSanPhamLTM = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
-            ViewBag.ListSP = listSanPhamLTM;
-            return View();
-        }
         [ChildActionOnly]
-        public ActionResult SanPhamPartial()
+        public ActionResult SanPhamStyle1Partial()
         {
-            //var listSanPhamLTM = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
-            //return PartialView(listSanPhamLTM);
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult SanPhamStyle1Partia2()
+        {
             return PartialView();
         }
     }
