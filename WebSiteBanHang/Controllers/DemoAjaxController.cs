5db61c7eb62c9e47d2b4c2b5f86ee37eed2e4442
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-03 10:20:32 +0700

    finish 4.3.1 Ajax ---  Load PartialView bang AjaxActionLink

---
+++ WebSiteBanHang/Controllers/DemoAjaxController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+
+namespace WebSiteBanHang.Controllers
+{
+    public class DemoAjaxController : Controller
+    {
+        QuanLyBanHangEntities db =  new QuanLyBanHangEntities();
+        // GET: DemoAjax
+        public ActionResult DemoAjax()
+        {
+            return View();
+        }
+        //Xu ly Action Link
+        public  ActionResult LoadAjaxActionLink()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return Content("Hello AJAX");
+        }
+
+        public ActionResult LoadAjaxBeginForm(FormCollection f)
+        {
+            string KQ = f["txt1"].ToString();
+            return Content(KQ);
+        }
+
+        public ActionResult LoadAjaxJQuery(int a, int b)
+        {
+            System.Threading.Thread.Sleep(2000);
+            return Content((a + b).ToString());
+        }
+
+        public ActionResult LoadSanPham()
+        {
+            //var listSanPhamLTM = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
+            //return PartialView(listSanPhamLTM);
+            var listSanPham = db.SanPhams;
+            return PartialView(listSanPham);
+        }
+    }
+}
