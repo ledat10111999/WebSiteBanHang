ee558965df7e16c5c3e98eb662d7997aba98719e
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-05 13:04:45 +0700

    fnish bai 22 (1/6)--- Bài 5.7- Hướng dẫn xây dựng chức năng giỏ hàng, đặt hàng… Note: --Tạo ViewModel GioHang  --> using bên trang XemGioHang -- Tách icon Giỏ hàng thành Partial View -- Nếu Partial View gọi từ Controller thì phải RETURN PartialView() --Edit lại GioHangPartial ( sửa link ) -- Chèn lại @Html.Action vào HeaderTopPartial sau khi tách ICON

---
+++ WebSiteBanHang/Controllers/GioHangController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteBanHang.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        public ActionResult XemGioHang()
+        {
+            return View();
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ WebSiteBanHang/Models/GioHang.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteBanHang.Models
+{
+    public class GioHang
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal ThanhTien { get; set; }
+        public string HinhAnh { get; set; }
+
+        public GioHang()
+        {
+
+        }
+
+        public GioHang(int iMaSP)
+        {
+            using (QuanLyBanHangEntities db = new QuanLyBanHangEntities())
+            {
+                this.MaSP = iMaSP;
+                SanPham sp = db.SanPhams.Single(n => n.MaSP == iMaSP);
+                this.TenSP = sp.TenSP;
+                this.DonGia = sp.DonGia.Value;
+                this.HinhAnh = sp.HinhAnh;
+                this.ThanhTien = DonGia * SoLuong;
+
+            }
+        }
+        public GioHang(int iMaSP,int sl)
+        {
+            using (QuanLyBanHangEntities db = new QuanLyBanHangEntities())
+            {
+                this.MaSP = iMaSP;
+                SanPham sp = db.SanPhams.Single(n => n.MaSP == iMaSP);
+                this.TenSP = sp.TenSP;
+                this.DonGia = sp.DonGia.Value;
+                this.HinhAnh = sp.HinhAnh;
+                this.SoLuong = sl;
+                this.ThanhTien = DonGia * SoLuong;
+
+            }
+
+            
+        }
+
+    }
+}
