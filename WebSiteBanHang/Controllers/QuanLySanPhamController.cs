515dda56027fab151b844bc0005c8220f8b6bc80
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-07 18:20:50 +0700

    finish bai 27 -- Bài 5.13b- Hướng dẫn xây dựng chức năng quản lý sản phẩm (Thêm, xóa, sửa) sản phẩm. + 1 phần chèn DatePicker bài 28: Note: --Tạo controller QuanLySanPham -- Xuất dữ liệu sản phẩm bằng control WEbgrid -- Action TaoMoi HttpGet ... tạo bằng template create ... Đổ dữ liệu bằng dropdownlist ..chú ý thuộc tính Name -- Chèn datePicker ...download source ...( từ khóa DatePicker Jquery)

---
+++ WebSiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+namespace WebSiteBanHang.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: QuanLySanPham
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.Where(n=>n.DaXoa==false));
+        }
+        [HttpGet]
+        public ActionResult TaoMoi()
+        {
+            //Load dropdownlist nhà cung cấp và loại sản phẩm
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
+            return View();
+        }
+    }
+}
