784b1723b39adbc1963a21ade4f94fdde55314cd
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-07 18:52:07 +0700

    commit lan 1 ---  bai 28 Thêm tính năng thêm hình ảnh vào thư mục @enctype="multipart/form-data"

--- WebSiteBanHang/Controllers/QuanLySanPhamController.cs
+++ WebSiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +22,35 @@ namespace WebSiteBanHang.Controllers
             ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
             ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai");
             ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TaoMoi(HttpPostedFileBase HinhAnh)
+        {
+
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX");
+
+            //Kiểm tra hình tồn tại trong csdl chưa 
+            if(HinhAnh.ContentLength > 0)
+            {
+                // Lấy tên hình
+                var fileName = Path.GetFileName(HinhAnh.FileName);
+                // Lấy hình ảnh chèn vào thư mục hình ảnh
+                var path = Path.Combine(Server.MapPath("~/Content/HinhAnhSP"), fileName);
+                //Nếu thư mục chứa hình ảnh rồi thì xuất ra thông báo
+                if (System.IO.File.Exists(path))
+                {
+                    ViewBag.upload = "Hình đã tồn tại!";
+                }
+                else
+                {
+                    HinhAnh.SaveAs(path);
+                }
+            }
+
             return View();
         }
     }
