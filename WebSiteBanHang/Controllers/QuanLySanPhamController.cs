13066a394b7d51b848c62ba3dd160485d557d9e8
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-08 10:19:03 +0700

    finish bai 29 -- Bài 5.15- Hướng dẫn xây dựng chức năng quản lý sản phẩm (Thêm, xóa, sửa) sản phẩm (tiếp theo). Note: -- chỉnh sửa css cho WEbgrid -- Chức năng chỉnh sửa  ... copy từ TaoMoi ....                   @Html.Hidden("MaSP",Model.MaSP) hiện mô tả                         <textarea name="MoTa" class="textarea">@Model.MoTa</textarea> hiện hình ảnh                     <img src="@Url.Content("~/Content/HinhAnhSP/" + Model.HinhAnh)" style="width:250px;height:150px"/> hiện dropdownlist               ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", sp.MaNCC);                                              ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.MaLoaiSP), "MaLoaiSP", "TenLoai", sp.MaLoaiSP);                                            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.MaNSX), "MaNSX", "TenNSX", sp.MaNSX);      Bổ sung chức năng nhỏ kiểm tra hình ảnh      Bổ sung valid meta data để hoàn thiện ChinhSua --- Chức năng xóa sản phẩm .... copy từ ChinhSua

--- WebSiteBanHang/Controllers/QuanLySanPhamController.cs
+++ WebSiteBanHang/Controllers/QuanLySanPhamController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebSiteBanHang.Models;
@@ -56,6 +57,94 @@ namespace WebSiteBanHang.Controllers
             db.SaveChanges();
 
             return View();
+        }
+        [ValidateInput(false)]
+        [HttpGet]
+        public ActionResult ChinhSua(int? id)
+        {
+            if (id == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
+            if(sp == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            //Load dropdownlist nhà cung cấp và loại sản phẩm
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC",sp.MaNCC);
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai",sp.MaLoaiSP);
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX",sp.MaNSX);
+
+            return View(sp);
+        }
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult ChinhSua(SanPham model)
+        {
+            //Load dropdownlist nhà cung cấp và loại sản phẩm
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", model.MaNCC);
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.TenLoai), "MaLoaiSP", "TenLoai", model.MaLoaiSP);
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.TenNSX), "MaNSX", "TenNSX", model.MaNSX);
+
+            db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+            // Su dung neu cai dat valid ben meta data day du
+            //if (ModelState.IsValid)
+            //{
+            //    db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //}
+            //return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Xoa(int? id)
+        {
+
+            //Lấy sản phẩm cần chỉnh sửa dựa vào id
+            if (id == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Load dropdownlist nhà cung cấp và dropdownlist loại sp, mã nhà sản xuất
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", sp.MaNCC);
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.MaLoaiSP), "MaLoaiSP", "TenLoai", sp.MaLoaiSP);
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.MaNSX), "MaNSX", "TenNSX", sp.MaNSX);
+            return View(sp);
+        }
+
+        [HttpPost]
+        public ActionResult Xoa(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham model = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            db.SanPhams.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
