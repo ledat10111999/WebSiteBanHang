539dba5d013945c9c0824fe0c4e7219872c93281
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-10 15:48:23 +0700

    finish bai 30 (1/4)  -- Bài 5.17- Hướng dẫn xây dựng chức năng nhập hàng( tiếp theo P.2) Note: -- Controller QuanLyPhieuNhap ...tạo chức năng nhập hàng ...bắt dữ liệu model và list model bằng jquery -- tạo dropdownlist bằng vòng lặp                          <select class="MaNCC" name="MaNCC">                     @foreach(var item in lstNCC)                     {                         <option value="@item.MaNCC">@item.TenNCC</option>                     }                 </select> -- Lấy giá trị value bằng thuộc tính NAME: theo model:                 <select class="MaNCC" name="MaNCC">                 <input type="text" name="NgayNhap" id="datepicker"> theo list model                 <select class="ddlSanPham" name="">                 <input name="" class="txtDonGia" value="0"/>                 <input name="" class="txtSoLuong" value="0"/> vd:          <input name="@Url.Content("[" +i+ "].MaSP")" value="@i"/><br />         <input name="@Url.Content("[" +i+ "].DonGiaNhap")" value="10000" /><br />         <input name="@Url.Content("[" +i+ "].SoLuongNhap")" value="100" /><br />

---
+++ WebSiteBanHang/Controllers/QuanLyPhieuNhapController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+namespace WebSiteBanHang.Controllers
+{
+    public class QuanLyPhieuNhapController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: QuanLyPhieuNhap
+        [HttpGet]
+        public ActionResult NhapHang()
+        {
+            ViewBag.MaNCC = db.NhaCungCaps;
+            ViewBag.ListSanPham = db.SanPhams;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NhapHang(PhieuNhap model,IEnumerable<ChiTietPhieuNhap> lstModel)
+        {
+            return View();
+        }
+    }
+}
