ee26afc203a7e4d7d9741fb2896a0492429e183e
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-10 16:29:17 +0700

    commit lan 2: đưa dữ liệu Phiếu nhập và chi tiết phiếu nhập vào database

--- WebSiteBanHang/Controllers/QuanLyPhieuNhapController.cs
+++ WebSiteBanHang/Controllers/QuanLyPhieuNhapController.cs
@@ -21,6 +21,25 @@ namespace WebSiteBanHang.Controllers
         [HttpPost]
         public ActionResult NhapHang(PhieuNhap model,IEnumerable<ChiTietPhieuNhap> lstModel)
         {
+
+            ViewBag.MaNCC = db.NhaCungCaps;
+            ViewBag.ListSanPham = db.SanPhams;
+            // Kiểm tra dữ liệu đầu vào bằng javascript hay bên metadata đều được
+            // Phải ktra để khớp với kiểu dữ liệu của database
+
+            //Gán đã xóa = false
+            model.DaXoa = false;
+            db.PhieuNhaps.Add(model);
+            db.SaveChanges();
+            // SaveChanges lần đầu để  sinh ra mã phiếu nhập gán cho lstChiTietPhieuNhap
+            foreach(var item in lstModel)
+            {
+                // Gán mã phiếu nhập cho từng chi tiết phiếu nhập
+                item.MaPN = model.MaPN;
+            }
+            // lệnh gán theo list
+            db.ChiTietPhieuNhaps.AddRange(lstModel);
+            db.SaveChanges();
             return View();
         }
     }
