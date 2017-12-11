0a1a8b6f4d3cb5016eba332255d162bebd27a1e4
ducnghia1205 <ducnghia1205@gmail.com>
2017-12-11 15:08:54 +0700

    finish bai 34 -- Bài 5.20- Hướng dẫn xây dựng chức năng thống kê Note: thông kê theo tháng năm....lọc điều kiện rồi duyệt từng đơn hàng tính tổng chi tiết đơn hàng

---
+++ WebSiteBanHang/Controllers/ThongKeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+namespace WebSiteBanHang.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: ThongKe
+        public ActionResult Index()
+        {
+            ViewBag.TongDoanhThu = ThongKeDoanhThu();
+            ViewBag.TongDDH = ThongKeDonHang();
+            ViewBag.TongThanhVien = TongThanhVien();
+            ViewBag.TongDoanhThuTheoThang = ThongKeDoanhThuTheoThang(12, 2017);
+            return View();
+        }
+
+        public decimal? ThongKeDoanhThu()
+        {
+            decimal? TongDoanhThu =db.ChiTietDonDatHangs.Sum(n => n.DonGia * n.SoLuong);
+            return TongDoanhThu;
+        }
+
+        public double ThongKeDonHang()
+        {
+            //Đếm đơn đặt hàng
+            double sl = db.DonDatHangs.Count();
+            return sl;
+        }
+
+        public double TongThanhVien()
+        {
+            double slTV = db.ThanhViens.Count();
+            return slTV;
+        }
+
+        public decimal? ThongKeDoanhThuTheoThang(int Thang, int Nam)
+        {
+            var lstDDH = db.DonDatHangs.Where(n=>n.NgayDat.Value.Month==Thang && n.NgayDat.Value.Year==Nam);
+            decimal? TongTien = 0;
+            //Duyệt chi tiết đơn hàng theo điều kiện
+            foreach(var item in lstDDH)
+            {
+                TongTien += item.ChiTietDonDatHangs.Sum(n => n.DonGia * n.SoLuong);
+            }
+            return TongTien;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                    db.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
